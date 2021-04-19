76fa8fed1cb605fba7bd60206f3dbf65041ebe3a
Brian <bdombro@gmail.com>
2021-04-18 19:21:59 -0500

    Aurora serverless working

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const {dbArn, dbSecretArn, dbName} = process.env
+
 const fastify = require('fastify')({
   logger: true
 })
@@ -6,8 +8,19 @@ fastify.get('/', (request, reply) => {
   reply.send({ hello: 'world' })
 })
 
-fastify.get('/inner', (request, reply) => {
-  reply.send({ hello: 'inner' })
+fastify.get('/config', (request, reply) => {
+  reply.send({ dbArn, dbSecretArn, dbName })
+})
+
+fastify.get('/dbcheck', async (request, reply) => {
+  const data = require('data-api-client')({
+    secretArn: dbSecretArn,
+    resourceArn: dbArn,
+    database: dbName,
+  })
+
+  let result = await data.query(`SHOW DATABASES`)
+  reply.send(result)
 })
 
 module.exports = fastify
