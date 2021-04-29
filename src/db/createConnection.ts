41c3bce5db31299c48a4e1eb1c6a0729329e6306
Brian <bdombro@gmail.com>
2021-04-29 09:37:37 -0500

    Tidy createConnection

--- src/db/createConnection.ts
+++ src/db/createConnection.ts
@@ -1,8 +1,13 @@
-import { Connection, createConnection as _createConnection } from 'typeorm'
+import type { ConnectionOptions } from 'typeorm'
+import { createConnection as tCreateConnection } from 'typeorm'
 
 import env from '../lib/env'
 
-export default function(): Promise<Connection> {
+/**
+ * Calls a memoized typeorm.createConnection. By memoizing, it can be 
+ * called from anywhere without worrying about opening multiple connections.
+ */
+export default function createConnection() {
 	return createConnectionM()
 		.catch(e => {
 			createConnectionM.bust()
@@ -10,25 +15,34 @@ export default function(): Promise<Connection> {
 		})
 }
 
-async function createConnection(): Promise<Connection> {
-	return (env.isProd 
-		? _createConnection({
-			type: 'aurora-data-api',
-			region: env.region,
-			secretArn: env.dbSecretArn,
-			resourceArn: env.dbArn,
-			database: env.dbName,
-			serviceConfigOptions: { maxRetries: 8 },
-			// synchronize: true,
-			migrationsRun: true,
-			entities: 		['src/db/entity/**/entity.js'],
-			migrations: 	['src/db/migration/**/*.js'],
-			subscribers: 	['src/db/subscriber/**/*.js'],
-		}) 
-		: _createConnection()
-	)
-	// .catch(() => {
-	// 	throw new Error('DB connect failed.')
-	// })
+/**
+ * Calls typeorm.createConnection with the proper config. On local, Typeorm
+ * will just check <root>/ormconfig.js.
+ * 
+ * I tried to add logic in ormconfig.js to switch based on env, but Lambda 
+ * barfed for some reason.
+ */
+async function createConnectionRaw() {
+	return env.isProd ? tCreateConnection(prodOrmconfig) : tCreateConnection()
 }
-const createConnectionM = Function.memoize(createConnection)
+
+/**
+ * Memoize createConnection so that it can be called from anywhere without
+ * worrying about opening multiple connections.
+ */
+const createConnectionM = Function.memoize(createConnectionRaw)
+
+
+const prodOrmconfig: ConnectionOptions = {
+	type: 'aurora-data-api',
+	region: env.region,
+	secretArn: env.dbSecretArn,
+	resourceArn: env.dbArn,
+	database: env.dbName,
+	serviceConfigOptions: { maxRetries: 8 },
+	// synchronize: true,
+	migrationsRun: true,
+	entities: 		['src/db/entity/**/entity.js'],
+	migrations: 	['src/db/migration/**/*.js'],
+	subscribers: 	['src/db/subscriber/**/*.js'],
+}
